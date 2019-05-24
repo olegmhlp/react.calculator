0acbf3e7e1080879ab1fd6c06d95ffc3d485ac01
opsudana <opsudana@gmail.com>
2019-05-24 15:32:48 +0300

    all

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       isLoggedIn: false
+//     };
+//     this.logButton = this.logButton.bind(this);
+//   }
+
+//   logButton() {
+//     this.setState(prevState => {
+//         return{
+//             isLoggedIn: !prevState.isLoggedIn
+//         }
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>
+//           User logged
+//           {this.state.isLoggedIn ? ' in' : ' out'}
+//         </h1>
+//         <button onClick={this.logButton}>
+//           {this.state.isLoggedIn ? 'LOG OUT' : 'LOG IN'}
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+
+import React from 'react';
+import {LinkedCalendar} from 'D:/Downloads/rb-datepicker-master/rb-datepicker-master/src';
+
+class App extends React.Component {
+    onDatesChange = ({ startDate, endDate }) => {
+        console.log(({ startDate, endDate }));
+    }
+    render() {
+        return (
+            <LinkedCalendar onDatesChange={this.onDatesChange} showDropdowns={false} />
+        );
+    }
 }
 
-export default App;
+export default App
