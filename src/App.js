688b0f458f7aed1f993c6332e07590573396ab56
opsudana <opsudana@gmail.com>
2019-06-10 16:32:04 +0300

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,48 @@
 import React from 'react';
-import './App.css'
+import './App.css';
 import Display from './components/Display';
 import ButtonsPanel from './components/ButtonsPanel';
-import operations from './components/operations'
+import operations from './components/operations';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        current: 0,
-        next: 0,
+      current: 0,
+      next: 0,
       operation: null
     };
     this.onClick = this.onClick.bind(this);
   }
 
   onClick(buttonValue) {
-     if(+buttonValue || buttonValue === 0){
-         this.setState({current: buttonValue })        
-     } else (this.setState({operation: buttonValue}))
-      
-    this.setState(operations(this.state));    
-  }
+    if (+buttonValue || buttonValue == 0) {
+      this.setState(prevState => ({
+        current: prevState.current + buttonValue
+      }));
+    } else {
+      this.setState({
+        operation: buttonValue
+      });
+    }
 
+    if (buttonValue === 'AC') {
+      this.setState({ current: 0 });
+    }
+    console.log(
+      'Current: ' +
+        this.state.current +
+        '/n' +
+        'Operation: ' +
+        this.state.operation
+    );
+  }
 
   render() {
     return (
       <div className="calculator">
         <Display current={this.state.current} />
-        <ButtonsPanel onClick={this.onClick} />       
+        <ButtonsPanel onClick={this.onClick} />
       </div>
     );
   }
--- src/components/Button.js
+++ src/components/Button.js
@@ -2,13 +2,11 @@ import React, { Component } from 'react';
 import './Button.css';
 
 class Button extends Component {
-  
-
   render() {
     return (
       <div className="buttonComponent">
         <button
-        className={this.props.className}
+          className={this.props.className}
           onClick={e => this.props.onClick(e.target.value)}
           value={this.props.value}
         >
