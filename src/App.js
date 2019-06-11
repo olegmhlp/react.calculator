6af1a38572524389b987d765eeb6134e1dbcc5a6
opsudana <opsudana@gmail.com>
2019-06-11 11:58:41 +0300

    add: badly worked calculations

--- src/App.js
+++ src/App.js
@@ -3,42 +3,69 @@ import './App.css';
 import Display from './components/Display';
 import ButtonsPanel from './components/ButtonsPanel';
 import operations from './components/operations';
+import { isNumber } from 'util';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      result: 0,
       current: 0,
       next: 0,
       operation: null
     };
-    this.onClick = this.onClick.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  onClick(buttonValue) {
-      this.setState({current: this.state.current + buttonValue})
-      if(this.state.current.toString().length >= 8){
-          document.getElementById('displayText').style.fontSize = '25px';
+  handleClick(buttonValue) {
+    // this.setState({current: this.state.current + buttonValue})
+    if (this.state.current.toString().length >= 8) {
+      document.getElementById('displayText').style.fontSize = '25px';
+    }
+
+    if (!this.state.operation) {
+      if (+buttonValue || buttonValue == 0) {
+        this.setState({
+          current: this.state.current + buttonValue,
+          result: this.state.result + buttonValue
+        });
+      } else {
+        this.setState({
+          operation: buttonValue,
+          result: this.state.result + buttonValue
+        });
       }
-    // if (+buttonValue || buttonValue == 0) {
-    //   this.setState(prevState => ({
-    //     current: prevState.current + buttonValue
-    //   }));
-    // } else {
-    //   this.setState({
-    //     operation: buttonValue
-    //   });
-    // }
+    } else {
+      this.setState({
+        next: this.state.next + buttonValue,
+        result: this.state.result + buttonValue
+      });
+    }
 
     if (buttonValue === 'AC') {
-      this.setState({ 
-          current: 0,
-        operation: null });
+      this.setState({
+        current: 0,
+        next: 0,
+        result: 0,
+        operation: null
+      });
+    }
+
+    if (buttonValue === '=') {
+      const resultCalc = operations(
+        +this.state.current,
+        +this.state.next,
+        this.state.operation
+      );
+      this.setState({ result: resultCalc });
     }
     console.log(
       'Current: ' +
         this.state.current +
-        '/n' +
+        '\n' +
+        'Next: ' +
+        this.state.next +
+        '\n' +
         'Operation: ' +
         this.state.operation
     );
@@ -47,8 +74,8 @@ class App extends React.Component {
   render() {
     return (
       <div className="calculator">
-        <Display current={this.state.current} />
-        <ButtonsPanel onClick={this.onClick} />
+        <Display current={this.state.result} />
+        <ButtonsPanel onClick={this.handleClick} />
       </div>
     );
   }
