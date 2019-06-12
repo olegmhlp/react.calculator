c9dd117ef1aa4bccff6f45e92d8209de33cc76f5
opsudana <opsudana@gmail.com>
2019-06-12 15:52:09 +0300

    calculation work

--- src/App.js
+++ src/App.js
@@ -2,83 +2,32 @@ import React from 'react';
 import './App.css';
 import Display from './components/Display';
 import ButtonsPanel from './components/ButtonsPanel';
-import operations from './components/operations';
-import { isNumber } from 'util';
+import calculator from './calculate/calculator';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      result: 0,
-      current: 0,
-      next: 0,
-      operation: null
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(buttonValue) {
-    // this.setState({current: this.state.current + buttonValue})
-    if (this.state.current.toString().length >= 8) {
-      document.getElementById('displayText').style.fontSize = '25px';
+    constructor(props) {
+        super(props);
+        this.state = {
+            displayVal: '0',
+            firstOperand: null,
+            isOperator: false,
+            operator: null
+        };
+        this.handleClick = this.handleClick.bind(this);
     }
 
-    if (!this.state.operation) {
-      if (+buttonValue || buttonValue == 0) {
-        this.setState({
-          current: this.state.current + buttonValue,
-          result: this.state.result + buttonValue
-        });
-      } else {
-        this.setState({
-          operation: buttonValue,
-          result: this.state.result + buttonValue
-        });
-      }
-    } else {
-      this.setState({
-        next: this.state.next + buttonValue,
-        result: this.state.result + buttonValue
-      });
+    handleClick(buttonValue) {
+        this.setState(calculator(this.state, buttonValue));
     }
 
-    if (buttonValue === 'AC') {
-      this.setState({
-        current: 0,
-        next: 0,
-        result: 0,
-        operation: null
-      });
+    render() {
+        return (
+            <div className="calculator">
+                <Display current={this.state.displayVal}/>
+                <ButtonsPanel onClick={this.handleClick}/>
+            </div>
+        );
     }
-
-    if (buttonValue === '=') {
-      const resultCalc = operations(
-        +this.state.current,
-        +this.state.next,
-        this.state.operation
-      );
-      this.setState({ result: resultCalc });
-    }
-    console.log(
-      'Current: ' +
-        this.state.current +
-        '\n' +
-        'Next: ' +
-        this.state.next +
-        '\n' +
-        'Operation: ' +
-        this.state.operation
-    );
-  }
-
-  render() {
-    return (
-      <div className="calculator">
-        <Display current={this.state.result} />
-        <ButtonsPanel onClick={this.handleClick} />
-      </div>
-    );
-  }
 }
 
 export default App;
---
+++ src/calculate/calculator.js
@@ -0,0 +1,78 @@
+import operations from './operations';
+
+export default function calculator(stateObj, buttonValue) {
+  const { displayVal, firstOperand, isOperator, operator } = stateObj;
+
+  if (isNumVal(buttonValue)) {
+    console.log('Number: ' + buttonValue);
+
+    if (displayVal === '0' && (buttonValue === '00' || buttonValue === '0')) {
+      return {};
+    }
+
+    if (displayVal === '0') {
+      stateObj.isOperator = true;
+      return { displayVal: buttonValue };
+    } else {
+      stateObj.isOperator = true;
+      return { displayVal: displayVal + buttonValue };
+    }
+  }
+
+  if (isOperator) {
+    console.log('Operator: ' + buttonValue);
+    if (!firstOperand) {
+      stateObj.firstOperand = displayVal;
+      stateObj.operator = buttonValue;
+      stateObj.displayVal = '0';
+    }
+  }
+
+  //---------------------------------------------------------
+
+  if (buttonValue === 'AC') {
+    return {
+      displayVal: '0',
+      firstOperand: null,
+      isSecondOperandNeed: false,
+      operator: null
+    };
+  }
+
+  if (buttonValue === 'C') {
+    return { displayVal: '0' };
+  }
+
+  if (buttonValue === '+/-') {
+    return { displayVal: displayVal * -1 };
+  }
+
+  if (buttonValue === '.') {
+    if (!displayVal.includes('.')) {
+      return { displayVal: displayVal + '.' };
+    } else return {};
+  }
+
+  if (buttonValue === '=') {
+    const resultCalc = operations(+firstOperand, +displayVal, operator);
+    console.log(firstOperand + '\n' + displayVal + '\n' + operator);
+
+    return {
+      operator: null,
+      firstOperand: null,
+      displayVal: resultCalc
+    };
+  }
+
+  changeDisplayScale(displayVal);
+}
+
+function isNumVal(val) {
+  return /^\d+$/.test(val);
+}
+
+function changeDisplayScale(value) {
+  if (value.toString().length >= 8) {
+    document.getElementById('displayText').style.fontSize = '25px';
+  }
+}
