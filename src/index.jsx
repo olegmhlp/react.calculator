f6ef0a8ac0e4d72387ca5267a146f53c508b9a65
olegmhlp <aleh.mikhalap@itechart-group.com>
2022-01-15 22:25:46 +0300

    [DRAFT] one line

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import { calculator, buttons } from './helper';
+import { calc, buttons } from './helper';
 import './styles.css';
 
 const App = () => {
   const [state, setState] = useState({ display: '0' });
-  const onClick = ({ target: { value } }) =>
-    setState({ ...state, ...calculator(state, value) });
+  const onClick = (e) => setState({ ...state, ...calc(state, e) });
 
   return buttons.map((i) => (
     <input type="button" onClick={onClick} value={state[i] || i} key={i} />
